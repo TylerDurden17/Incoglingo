8f2706b5d7745644da207bb287a498e75b6b49ff
TylerDurden17 <kartikrai14@gmail.com>
2023-12-17 15:51:50 +0530

    countdown

--- client/src/components/timeZone.js
+++ client/src/components/timeZone.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment-timezone';
+import Countdown from 'react-countdown';
 
 const MeetingTime = () => {
   const [meetingTime, setMeetingTime] = useState('');
   const [zone, setZone] = useState('');
+
+    
+  // Calculate the target time for 6:30pm UTC
+  const targetTime = new Date();
+  targetTime.setUTCHours(13, 0, 0, 0);
+
+  const CountdownRenderer = ({ days, hours, minutes, seconds }) => {
+    return (
+      <span style={{ color: 'green' }}>
+        session in {days} days {hours} hours {minutes} minutes {seconds} seconds
+      </span>
+    );
+  };
   
   useEffect(() => {
     //explicitly specify the time zone when creating the initial moment object
@@ -19,7 +33,8 @@ const MeetingTime = () => {
 
   return (
     <>
-      <p> Talk at: <b>{meetingTime}</b> ({zone} timezone)</p>
+      <div> Talk at: <b>{meetingTime}</b> ({zone} timezone)</div>
+      <Countdown date={targetTime} renderer={CountdownRenderer} />
     </>
   );
 };
