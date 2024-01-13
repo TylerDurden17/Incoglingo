4425788626dcebad9f1cca96d9713b57c6b1005d
TylerDurden17 <kartikrai14@gmail.com>
2024-01-13 16:18:41 +0530

    final

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -53,20 +53,19 @@ useEffect(()=>{
       path: "/"
     });
 
-    const newSocket = io("http://localhost:8080", {
-      transports : ["websocket", "polling"],
-       reconnectionDelay: 1000, // defaults to 1000
-       reconnectionDelayMax: 5000, // defaults to 5000
-       reconnectionAttempts: 5
-    });
-    // const newSocket = io('https://incoglingo.onrender.com/', {
-    //   transports : ['websocket', 'polling'],
-    //   reconnection: true, // Enable reconnection
-    //   reconnectionAttempts: 5, // Number of reconnection attempts
-    //   reconnectionDelay: 1000, // Delay between reconnection attempts in milliseconds
-    //   reconnectionDelayMax: 5000, // Maximum delay between reconnection attempts
+    // const newSocket = io("http://localhost:8080", {
+    //   transports : ["websocket", "polling"],
+    //    reconnectionDelay: 1000, // defaults to 1000
+    //    reconnectionDelayMax: 5000, // defaults to 5000
+    //    reconnectionAttempts: 5
     // });
-
+    const newSocket = io('https://incoglingo.onrender.com/', {
+      transports : ['websocket', 'polling'],
+      reconnection: true, // Enable reconnection
+      reconnectionAttempts: 5, // Number of reconnection attempts
+      reconnectionDelay: 1000, // Delay between reconnection attempts in milliseconds
+      reconnectionDelayMax: 5000, // Maximum delay between reconnection attempts
+    });
 
     newPeer.on("open", (id) => {
       newSocket.emit("join-room", roomId, id, name);
