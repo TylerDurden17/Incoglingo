1734063ff61efbe8d75501e29e493caf37b242b9
TylerDurden17 <kartikrai14@gmail.com>
2024-05-09 23:44:51 +0530

    fixed .env

--- client/src/auth/firebaseConfig.jsx
+++ client/src/auth/firebaseConfig.jsx
@@ -5,14 +5,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
-console.log(firebaseConfig);
 
 export default firebaseConfig;
